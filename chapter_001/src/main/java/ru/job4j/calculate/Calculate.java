dd64606a4a9f0a657629c1ea13fc06f826711249
DavudUtsumuev <devito.0586@gmail.com>
2019-05-02 17:41:14 +0500

    1.4. JUnit[#135450]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,6 +3,8 @@ package ru.job4j.calculate;
 * Calculate.
 *
 * @author Davud Utsumuev (dav.u2015@yandex.ru)
+* @version $Id$ 
+* @since 02.05.2019
 */
 public class Calculate {
 /**
@@ -11,5 +13,13 @@ public class Calculate {
 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+	}
+	/** 
+	* Method echo.
+	* @param name Davud Utsumuev.
+	* @return Echo plus Davud Utsumuev.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : Davud Utsumuev";
 	}
 }
---
+++ chapter_001/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Davud Utsumuev (dav.u2015@yandex.ru)
+ * @version 1.3
+ * @since 02.05.2019
+ */
+public class CalculateTest {
+ /**
+ * Test echo.
+ */
+
+@Test
+ public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Davud Utsumuev";
+    String expect = "Echo, echo, echo : Davud Utsumuev"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+ }
+
+}
